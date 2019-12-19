f58caf81a4298ff6c4201f5af5887543f9c1a048
0verM1nd <41874423+0verM1nd@users.noreply.github.com>
2019-12-19 14:49:06 +0300

    Update Project.java

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -8,12 +8,7 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
-  {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
+  
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
