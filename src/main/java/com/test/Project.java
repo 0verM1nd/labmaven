fe5efdc6c2f29455bab650bd3e474cc5c9a5e230
Sergey Romanchenko <sergey_romanchenko@epam.com>
2019-12-19 06:30:40 -0500

    add some str

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -14,3 +14,22 @@ class Project {
   }
 
 }
+public final class SpringCli {
+
+	private SpringCli() {
+	}	public static void main(String... args) {
+		System.setProperty("java.awt.headless", Boolean.toString(true));
+		LogbackInitializer.initialize();
+
+		CommandRunner runner = new CommandRunner("spring");
+		ClassUtils.overrideThreadContextClassLoader(createExtendedClassLoader(runner));
+		runner.addCommand(new HelpCommand(runner));
+		addServiceLoaderCommands(runner);
+		runner.addCommand(new ShellCommand())
+		runner.addCommand(new HintCommand(runner))
+		runner.setOptionCommands(HelpCommand.class, VersionCommand.class);
+		runner.setHiddenCommands(HintCommand.class)
+		int exitCode = runner.runAndHandleErrors(args);
+		if (exitCode != 0) {
+			// If successful, leave it to run in case it's a server app
+			System.exit(exitCode);	}}
