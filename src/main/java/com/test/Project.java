180c5847ba621739fdf5731d65be54959e20d80e
0verM1nd <41874423+0verM1nd@users.noreply.github.com>
2019-12-19 15:06:14 +0300

    Update Project.java

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -7,6 +7,8 @@ class Project {
 
   public static void test() {
     System.out.println("test");
+    System.out.println("test1");
+    System.out.println("test2");
   }
   
   public static void main(String[] args) {
